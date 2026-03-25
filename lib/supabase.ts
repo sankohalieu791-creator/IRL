8210943946b42b47a9e46e492abcd00a6d91748d
Alieu Sankoh <sankohalieu791@gmail.com>
2026-03-25 20:09:58 +0000

    Fix supabase connection

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = "https://rowmmeqmskryjychlwtt.supabase.co"
+const supabaseKey = "sb_publishable_uKPueXBOZd7PopRqRySbNg_yIHzus_D"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
